bf12c0506a2815daa7c55f749afbb1e4c5595761
Jorge Contreras <86329049+jorgeostos@users.noreply.github.com>
2023-02-09 23:17:11 -0600

    started navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,7 +1,26 @@
+import {
+  Box,
+  Flex,
+  Spacer,
+  Button,
+  ButtonGroup,
+  Heading,
+} from "@chakra-ui/react";
 import React from "react";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <Flex minWidth="max-content" alignItems="center" gap="2" mt="14">
+      <Box p="2">
+        <Heading size="md">Chakra App</Heading>
+      </Box>
+      <Spacer />
+      <ButtonGroup gap="2">
+        <Button colorScheme="teal">Sign Up</Button>
+        <Button colorScheme="teal">Log in</Button>
+      </ButtonGroup>
+    </Flex>
+  );
 };
 
 export default Navbar;
